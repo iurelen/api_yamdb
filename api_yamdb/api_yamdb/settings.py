f9980bd50afdea9bb9ef2f1ddd9da0cc15400e17
Badmajor <89295663+Badmajor@users.noreply.github.com>
2024-01-02 20:01:34 +0300

    Revert "Feature/api Добавил вьюхи (пока для API), модели и эндпоинты для категорий, жанров, произведений, обзоров и комментов"

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -21,10 +21,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
     'api.apps.ApiConfig',
-    'users.apps.UsersConfig',
-    'reviews.apps.ReviewsConfig',
+    'users.apps.UsersConfig'
 ]
 
 MIDDLEWARE = [
