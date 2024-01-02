584add4003129e287b05bb7ec556946d467d40af
Badmajor <vtk88@mail.ru>
2024-01-02 22:08:11 +0300

    added fields role and bio into model CustomUser

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,9 +1,22 @@
 from django.contrib.auth.models import AbstractUser
-# from django.db import models
+from django.db import models
+
+ROLE_CHOICE =(
+    ('admin', 'Admin'),
+    ('user', 'User'),
+    ('moderator', 'Moderator'),
+    ('superuser', 'Superuser'),
+)
 
 
 class CustomUser(AbstractUser):
-    pass
+    role = models.CharField(
+        max_length=32,
+        choices=ROLE_CHOICE,
+    )
+    bio = models.TextField(
+        blank=True,
+    )
 
     def __str__(self):
         return self.username
