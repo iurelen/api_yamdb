2b5116c311556ad06022efcffb4f24279007ddd9
Badmajor <vtk88@mail.ru>
2024-01-11 16:23:37 +0300

    все пути v1/ теперь находятся в v1/urls.py

--- api_yamdb/api/v1/urls.py
+++ api_yamdb/api/v1/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from users.views import SignupView, UserViewSet, TokenObtainView
@@ -28,4 +28,9 @@ router_v1.register('users', UserViewSet, basename='users')
 auth_urls = [
     path('signup/', SignupView.as_view(), name='signup'),
     path('token/', TokenObtainView.as_view(), name='token_obtain'),
+]
+
+urlpatterns = [
+    path('', include(router_v1.urls)),
+    path('auth/', include(auth_urls)),
 ]
